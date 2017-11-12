5f5215fbc93c2776a2b68bf9364d513830d784d2
RaptorCentauri <dave.leo.shilander@icloud.com>
2017-11-12 14:05:11 -0600

    Really late initial commit

---
+++ liri.js
@@ -0,0 +1,105 @@
+
+//CHANGES!!!!!
+
+
+//Requirements
+const request = require('request');
+// const omdbKeys = require('omdbKeys');
+const Spotify = require('node-spotify-api');
+const spotifyKeys = require('./spotifyKeys');
+
+
+
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+
+
+//Variables
+let command = process.argv[2];
+let argument = process.argv[3];
+
+
+
+//The functionss
+function myTweets() {
+	console.log(`twitter`);
+}
+
+function spotifyThis() {
+	console.log('spotify');
+	spotify.search({ type: 'track', query: argument }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data);
+
+  });
+}
+
+function movieThis(){
+
+ let apiKey = `40e9cece`;
+
+  var queryURL = `http://www.omdbapi.com/?t="${argument}"&apikey=${apiKey}`;
+
+  request(queryURL, function (error, response, body){
+
+    if (!error && response.statusCode === 200){
+
+      let movieInfo = {
+        //Title
+        Title: JSON.parse(body).Title,
+        //Release Year
+        Year: JSON.parse(body).Year,
+        //IMDB Rating
+        ImdbRating: JSON.parse(body).Ratings[0].Value,
+        //Rotten Tomato Rating
+        RottenRating: JSON.parse(body).Ratings[1].Value,
+        //Countyr where move was produced
+        Country: JSON.parse(body).Country,
+        //Language
+        Language: JSON.parse(body).Language,
+        //Plot
+        Plot: JSON.parse(body).Plot,
+        //Actors
+        Actors: JSON.parse(body).Actors,
+      }
+
+      for (key in movieInfo){
+        console.log(`${key}: ${movieInfo[key]}`);
+      }
+
+    }
+
+    else {
+      console.log(`error: ${error}`);
+    }
+  })
+}
+
+function doIt(){
+	console.log('do it');
+}
+
+
+//Which command to run?
+switch (command) {
+	case 'my-tweets': myTweets();
+	break;
+
+	case 'spotify-this-song': spotifyThis();
+	break;
+
+	case 'movie-this': movieThis();
+	break;
+
+	case 'do-what-it-says': doIt();
+	break;
+
+	default:
+
+}
